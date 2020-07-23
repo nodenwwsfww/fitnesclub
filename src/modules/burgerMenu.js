805e0382871cdd41b4dd5ce013ba4c4532ea393c
Roman Radchenko <nodenwwsfww@gmail.com>
2020-07-23 14:32:00 +0300

    popup бургер меню

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,4 +1,17 @@
 const burgerMenu = () => {
-    //
+    const popupMenu = document.querySelector(".popup-menu"),
+        menuButton = document.getElementById("menu-button-click");
+
+    menuButton.addEventListener("click", () => popupMenu.style.display = "flex");
+
+    popupMenu.addEventListener("click", event => {
+
+        const target = event.target;
+
+        if (target.closest(".close-menu-btn") || target.closest(".scroll")) {
+            popupMenu.style.display = "none";
+        }
+    });
+
 };
 export default burgerMenu;
