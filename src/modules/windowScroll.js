b68c7e7dcfe321e3a10e94429e27e6e347317814
Roman Radchenko <nodenwwsfww@gmail.com>
2020-07-23 15:48:30 +0300

    Скролл стрелка

--- src/modules/windowScroll.js
+++ src/modules/windowScroll.js
@@ -1,9 +1,12 @@
 const windowScroll = () => {
-    window.addEventListener("scroll", () => {
-        const burgerMenu = document.querySelector(".top-menu");
+    const burgerMenu = document.querySelector(".top-menu"),
+        arrowUp = document.getElementById("totop"),
+        firstBlock = document.querySelector(".head");
 
+    window.addEventListener("scroll", () => {
         if (pageYOffset > 0) {
             burgerMenu.style.position = "fixed";
+            arrowUp.style.position = pageYOffset > firstBlock.clientHeight ? "fixed" : "relative";
         } else {
             burgerMenu.style.position = "relative";
         }
