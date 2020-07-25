fdfa2789897a880c09b39fa1f10faaae75623501
Roman Radchenko <nodenwwsfww@gmail.com>
2020-07-25 18:56:29 +0300

    Учёт промокода в калькуляторе

--- src/modules/paymentCalculater.js
+++ src/modules/paymentCalculater.js
@@ -3,15 +3,22 @@ const paymentCalculater = () => {
     const calcBlock = document.getElementById("calculater");
 
     const calcTotalPrice = () => {
+        let total = 0;
+
         const selectedClub = getChosenClub(calcBlock.querySelectorAll("input[name=\"club-name\"")),
             cards = [...calcBlock.querySelectorAll("input[name=\"card-type\"")],
             cardsPrice = (selectedClub === "mozaika" ? [1999, 9900, 13900, 19900] : [2990, 14990, 21990, 24990]);
 
         /* Вычисление исходя из выбранной карточки */
-        return cardsPrice[ cards.findIndex(item => item.checked) ];
+        total += cardsPrice[ cards.findIndex(item => item.checked) ];
+
+        /* Скидка 30% при наличии промокода*/
+        total -= calcBlock.querySelector("#input-promocode").value.trim() === "ТЕЛО2020" ? total * (100 - 30) / 100 : 0;
+        return total;
     };
 
     calcBlock.addEventListener("change", () => {
+        console.log(event.target);
         let totalPrice;
         try {
             totalPrice = +calcTotalPrice();
