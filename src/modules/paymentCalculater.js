946573210a4f1ffd1a24b599db9e55cbc7a4e566
Roman Radchenko <nodenwwsfww@gmail.com>
2020-07-28 12:10:54 +0300

    округление в калькуляторе

--- src/modules/paymentCalculater.js
+++ src/modules/paymentCalculater.js
@@ -4,7 +4,7 @@ const paymentCalculater = () => {
 
     const calcTotalPrice = () => {
         let total = 0;
-        
+
         const selectedClub = getChosenClub(calcBlock.querySelectorAll("input[name=\"club-name\"")),
             cards = [...calcBlock.querySelectorAll("input[name=\"card-type\"")],
             cardsPrice = (selectedClub === "mozaika" ? [1999, 9900, 13900, 19900] : [2990, 14990, 21990, 24990]);
@@ -14,7 +14,7 @@ const paymentCalculater = () => {
 
         /* Скидка 30% при наличии промокода*/
         total -= calcBlock.querySelector("#input-promocode").value.trim() === "ТЕЛО2020" ? total * 30 / 100 : 0;
-        return total;
+        return Math.round(total);
     };
 
     calcBlock.addEventListener("change", () => {
